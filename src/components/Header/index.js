27a781f9da3fc4e3bbeaf7368e40ead7a3a5ac9f
Hoang Trinh <hoang.trinhj@gmail.com>
2020-11-21 10:31:10 +0700

    Update Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import AnchorLink from 'react-anchor-link-smooth-scroll';
-import MobileMenu from '../../components/MobileMenu';
 
+import MusicButton from '../MusicButton';
 import './style.css';
 
 const Header = () => {
@@ -11,42 +9,11 @@ const Header = () => {
       <div className="header">
         <div className="container">
           <div className="row">
-            <div className="col-lg-3 col-10">
-              <div className="logo">
-                <h2>
-                  <Link to="/">
-                    Hoàng
-                    <span>
-                      <i className="fa fa-heart" aria-hidden="true"></i>
-                    </span>
-                    Thủy
-                  </Link>
-                </h2>
-              </div>
-            </div>
+            <div className="col-lg-3 col-10"></div>
             <div className="col-lg-9">
               <div className="header-menu d-lg-block d-none">
-                <ul className="mobail-menu d-flex">
-                  <li>
-                    <AnchorLink href="#couple">Couple</AnchorLink>
-                  </li>
-                  <li>
-                    <AnchorLink href="#story">Story</AnchorLink>
-                  </li>
-                  <li>
-                    <AnchorLink href="#event">Events</AnchorLink>
-                  </li>
-                  <li>
-                    <AnchorLink href="#gallery">Gallery</AnchorLink>
-                  </li>
-                  <li>
-                    <AnchorLink href="#rsvp">Rsvp</AnchorLink>
-                  </li>
-                </ul>
+                <MusicButton />
               </div>
-            </div>
-            <div className="col-2">
-              <MobileMenu />
             </div>
           </div>
         </div>
