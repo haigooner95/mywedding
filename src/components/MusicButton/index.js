51e7ec5709b21cded7fe9def098e3352546a7fce
Hoang Trinh <hoang.trinhj@gmail.com>
2020-11-21 10:08:55 +0700

    Add Music Button

---
+++ src/components/MusicButton/index.js
@@ -0,0 +1,31 @@
+import React, { Fragment } from 'react';
+import useSound from 'use-sound';
+
+import './style.css';
+import music from '../../audio/beautifulInWhite.mp3';
+import playButton from '../../images/music-button/play.png';
+import pauseButton from '../../images/music-button/pause.png';
+
+const MusicButton = () => {
+  const [play, { pause, isPlaying }] = useSound(music);
+
+  const handleClick = () => {
+    if (isPlaying) {
+      pause();
+    } else {
+      play();
+    }
+  };
+
+  return (
+    <Fragment>
+      <button className="music-btn" onClick={handleClick}>
+        <span>{isPlaying ? <img src={pauseButton} /> : <img src={playButton} />}</span>
+        <span>Music</span>
+      </button>
+      <audio href="audio_tag" loop src={music} type="audio/mpeg" controls></audio>
+    </Fragment>
+  );
+};
+
+export default MusicButton;
