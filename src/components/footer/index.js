40439d3d6acdd9c51b72ad10408fea386694eb30
Hoang Trinh <hoang.trinhj@gmail.com>
2020-12-27 21:35:52 +0700

    Update Page

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
+
 import './style.css';
 
 const Footer = () => {
+  const { t } = useTranslation();
+
   return (
     <div className="footer-area">
       <div className="container">
         <div className="footer-content">
           <div className="content-sub">
-            <h2>Thank You</h2>
-            <span>For Being With Us</span>
+            <h2>{t('THANK_YOU')}</h2>
+            <span>{t('FOR_BEING_WITH_US')}</span>
           </div>
         </div>
       </div>
